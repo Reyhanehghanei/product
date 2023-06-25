568d8cbb33e188b90480e89197b1dddd50c6a229
Reyhanehghanei <regh9108@gmail.com>
2023-06-25 01:18:32 -0500

    jadid

--- my js.js
+++ my js.js
@@ -1,5 +1,5 @@
 $(function () {
-    $(".owl-carousel").owlCarousel({
+    $(".silaeder").owlCarousel({
         items : 4,
         rtl : true,
         margin : 10,
@@ -18,11 +18,74 @@ $(function () {
             768 : {
                 items : 7,
             }
+            ,
+            500 : {
+                items : 4,
+            }
         }
 
     });
- 
-    
+})
+
+$(function () {
+    $(".slider").owlCarousel({
+        rtl : true,
+        margin : 10,
+        loop : true,
+        nav : true,
+        navText : ['<i class="fa-solid fa-angle-right"></i>','<i class="fa-solid fa-angle-left"></i>'],
+        dots : true,
+        autoplay : true,
+        autoplayTimeout : 1000,
+        responsive : {
+            // breakpoint from 0 up
+            0 : {
+                items : 4,
+                dots : false
+            },
+            768 : {
+                items : 3,
+            },
+            500 : {
+                items : 1,
+            }
+            ,
+            400 : {
+                items : 1,
+            }
+        }
+
+    });
+})
 
 
+$(function () {
+    $(".mikap-slider").owlCarousel({
+        rtl : true,
+        margin : 10,
+        loop : true,
+        nav : true,
+        navText : ['<i class="fa-solid fa-angle-right"></i>','<i class="fa-solid fa-angle-left"></i>'],
+        dots : true,
+        autoplay : true,
+        autoplayTimeout : 1000,
+        responsive : {
+            // breakpoint from 0 up
+            0 : {
+                items :1 ,
+                dots : false
+            },
+            768 : {
+                items : 3,
+            },
+            500 : {
+                items : 1,
+            }
+            ,
+            400 : {
+                items : 1,
+            }
+        }
+
+    });
 })
